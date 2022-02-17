ab1b44ac4816194f05b9df0dc5f2a26bb0f0d895
JB <97062543@st.deltion.nl>
2022-02-17 10:29:58 +0100

    commentaarregels toegevoegt
    commentaaregels toegevoegt

--- opdrachten/mijninfo.js
+++ opdrachten/mijninfo.js
@@ -1,10 +1,16 @@
+//informatie over student in verschillende const. voor overzicht
 const voornaam = "Naam: Jelle";
 const achternaam= "bergsma";
 const studentennummer="studentennummer:97062543";
 const klasnummer="Klasnummer: XSDX1";
 
+//groep met de naam studenten informatie
 console.group("studenten informatie")
+//een console log voornaam+achternaam const
 console.log(voornaam,achternaam);
+//een console log studentennummer
 console.log(studentennummer);
+//een console log klasnummer
 console.log(klasnummer);
+//einde groep
 console.groupEnd();
