7c3335d321c60ab5f2834d56bfb4cf1f79d27c59
JB <97062543@st.deltion.nl>
2022-02-17 10:18:09 +0100

    mijninfo opdracht codebase
    codebase mijn info

---
+++ mijn info/mijninfo.js
@@ -0,0 +1,10 @@
+const voornaam = "Naam: Jelle";
+const achternaam= "bergsma";
+const studentennummer="studentennummer:97062543";
+const klasnummer="Klasnummer: XSDX1";
+
+console.group("studenten informatie")
+console.log(voornaam,achternaam);
+console.log(studentennummer);
+console.log(klasnummer);
+console.groupEnd();
