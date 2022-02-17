eecad1f9a9ffa47f8c901c9e569ff4b461a8fd52
JB <97062543@st.deltion.nl>
2022-02-17 12:09:47 +0100

    cirkel
    cirkel.js

---
+++ opdrachten/cirkel.js
@@ -0,0 +1,8 @@
+//variable user word diameter gevraagt
+user = prompt("diameter:");
+//variabele rekenen berekening van omtrek
+const rekenen=  user*Math.PI;
+//variable rekenen berekeing van oppervlakte
+const reken2= Math.pow(user,2)*Math.PI/4
+console.log("omtrek:",rekenen);
+console.log("oppervlakte:",reken2);
