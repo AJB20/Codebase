5a3756e46db07db995497d0cc43f4808031d0fe3
ppcxde <zmtm@tutanota.com>
2022-03-09 13:45:33 +0100

    oefenen
    oefening of dit werk anders iets bedenken

--- opdrachten/php/deelbaar.php
+++ opdrachten/php/deelbaar.php
@@ -1,5 +1,15 @@
 <?php
 
-$a = readline('Enter a string: ');
+$eerste_cijfer = readline('eerste cijfer (deeltal)');
+$tweede_cijfer = readline('tweede cijfer (deler)');
+$totaal = $eerste_cijfer/$tweede_cijfer;
+echo ($totaal);
+
+if ($totaal==0) {
+    echo ("deling klopt");
+}
+else {
+    echo("deling kan niet worden gedaan!");
+}
 
 ?>
