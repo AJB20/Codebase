871de5fb7e1d609c46e4c21c4d0f4aaf5862d330
ppcxde <zmtm@tutanota.com>
2022-03-09 13:56:51 +0100

    werkeend maar niet volledige test
    werkend maar test lukt niet

--- opdrachten/php/deelbaar.php
+++ opdrachten/php/deelbaar.php
@@ -3,13 +3,12 @@
 $eerste_cijfer = readline('eerste cijfer (deeltal)');
 $tweede_cijfer = readline('tweede cijfer (deler)');
 $totaal = $eerste_cijfer/$tweede_cijfer;
-echo ($totaal);
-
 if ($totaal==0) {
     echo ("deling klopt");
 }
 else {
-    echo("deling kan niet worden gedaan!");
+    echo $totaal;
+    echo("\n deling kan niet worden gedaan!");
 }
 
 ?>
