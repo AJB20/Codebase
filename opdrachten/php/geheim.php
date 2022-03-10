b600a10812030384880273171b9e2a62dee4d415
ppcxde <zmtm@tutanota.com>
2022-03-10 08:43:31 +0100

    muntjegooien en geheim
    muntje gooien nog niet af geheim wel?  niet dudielijk

---
+++ opdrachten/php/geheim.php
@@ -0,0 +1,21 @@
+<?php
+$userinput= readline("typ hier de code \n");
+   switch ($userinput) {
+        case "a";
+            echo ("b");
+            break;
+            case "b" :
+                echo ("c");
+            break;
+                case "d" :
+                    echo ("e");
+                    break;
+                    case "f" :
+                        echo ("g");
+                        break;
+                        case "h" :
+                            echo ("i");
+                            break;
+        }
+
+?>
